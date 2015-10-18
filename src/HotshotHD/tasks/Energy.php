a85e163fb557ee4b50451e91dd4cb17714583936
MajikalPayton <papaytongamergirl@gmail.com>
2015-10-18 01:53:12 -0500

    Mainly just style fixes.

---
+++ src/HotshotHD/tasks/Energy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HotshotHD;
+
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+
+class Energy extends PluginTask {
+
+    public function __construct(ExoSuit $plugin, Player $player) {
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+        $this->player = $player;
+    }
+
+    public function onRun($currentTick) {
+        if($this->plugin->config->get("Energy-Popup-Enabled") == true) {
+            $this->player->sendPopup("Energy:" . $this->plugin->getEnergy($this->player));
+        }
+    }
+}
---
+++ src/HotshotHD/tasks/EnergyReduce.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HotshotHD;
+
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+
+class EnergyReduce extends PluginTask {
+
+    public function __construct(ExoSuit $plugin, Player $player){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+        $this->player = $player;
+    }
+
+    public function onRun($currentTick){
+        $this->plugin->setEnergy($this->player, $this->plugin->getEnergy($this->player) - 1);
+        if($this->plugin->getEnergy($this->player) <= 0) {
+            $this->plugin->resetEnergy($this->player);
+            $this->plugin->cancelTask($this->plugin->tasks[$this->plugin->getEnergyReduceTask($this->player)->getTaskId()]);
+        }
+    }
+}
